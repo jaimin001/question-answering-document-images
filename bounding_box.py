e5852a876f7dbe00111fbd6f21b2d8bf996c5576
Jaimin Gajjar <hari@Jaimins-MacBook-Air.local>
2023-12-31 00:27:05 +0530

    used the tesseract code in the demo file

--- bounding_box.py
+++ bounding_box.py
@@ -5,67 +5,62 @@ import json
 import random
 import os
 
-# Load the image
-img = cv2.imread("ffdh0227_1.png")
 
-# Resize the image to 1024x1024
-resized_img = cv2.resize(img, (1024, 1024))
+def save_bounding_boxes(image_path, questions, save_json=True, show_img=False):
+    img = cv2.imread(image_path)
 
-# Perform OCR and get data
-d = pytesseract.image_to_data(resized_img, output_type=Output.DICT)
+    # resizing as the model is trained for 1024x1024 images only
+    resized_img = cv2.resize(img, (1024, 1024))
 
-# Initialize the result list
-result = []
+    d = pytesseract.image_to_data(resized_img, output_type=Output.DICT)
 
-# Create a dictionary for the current image
-image_result = {
-    "boxes": [],
-    "context": [],
-    "image_id": "image.png",
-    "qas": []
-}
+    result = []
 
-# Loop through the boxes and store the information
-n_boxes = len(d['level'])
-for i in range(n_boxes):
-    text = d['text'][i]
-    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+    image_result = {"boxes": [], "context": [], "image_id": "image.png", "qas": []}
 
-    # Add bounding box coordinates to the current image dictionary
-    image_result["boxes"].append([x, y, x + w, y + h])
+    # Loop through the boxes and store the information
+    n_boxes = len(d["level"])
+    for i in range(n_boxes):
+        text = d["text"][i]
+        (x, y, w, h) = (d["left"][i], d["top"][i], d["width"][i], d["height"][i])
 
-    # Add text to the context list
-    image_result["context"].append(text)
+        # Add bounding box coordinates to the current image dictionary
+        image_result["boxes"].append([x, y, x + w, y + h])
 
-    # Draw rectangle on the image
-    cv2.rectangle(resized_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Add text to the context list
+        image_result["context"].append(text)
 
-# Add Q&A information
-qid = f"{image_result['image_id']}-{random.randint(1, 100000)}"
-qa = {
-    "answer": [{
-        "answer_end": 0,
-        "answer_start": 0,
-        "text": ""
-    }],
-    "qid": qid,
-    "question": "hello there"
-}
-image_result["qas"].append(qa)
+        # Draw rectangle on the image
+        cv2.rectangle(resized_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# Add the current image dictionary to the result list
-result.append(image_result)
+    # Remove image extension from the file name
+    image_name = os.path.splitext(image_path)[-2]
 
-# Remove image extension from the file name
-image_name = os.path.splitext(image_result['image_id'])[0]
+    # Add Q&A information
+    for question in questions:
+        qid = f"{image_name}-{random.randint(1, 100000)}"
+        qa = {
+            "answer": [{"answer_end": 0, "answer_start": 0, "text": ""}],
+            "qid": qid,
+            "question": question,
+        }
+        image_result["qas"].append(qa)
 
-# Convert the result list to JSON
-json_result = json.dumps(result, indent=2)
+    # Add the current image dictionary to the result list
+    result.append(image_result)
 
-# Save the JSON result to a file
-with open(f"{image_name}.json", 'w') as json_file:
-    json_file.write(json_result)
+    # Convert the result list to JSON
+    json_result = json.dumps(result, indent=2)
 
-# Display the resized image with bounding boxes
-cv2.imshow('resized_img', resized_img)
-cv2.waitKey(0)
+    file_path = os.path.join(os.getcwd(), "bounding_box_outputs", f"{image_name}.json")
+    if save_json:
+        if not os.path.exists("bounding_box_outputs"):
+            os.makedirs("bounding_box_outputs")
+        with open(file_path, "w") as json_file:
+            json_file.write(json_result)
+
+    if show_img:
+        cv2.imshow("resized_img", resized_img)
+        cv2.waitKey(0)
+
+    return file_path
